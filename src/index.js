a7f8de9974d6ceccc7d0937d57f06d99f52e9688
Nia <xn1a@protonmail.com>
2019-01-11 17:32:44 +0100

    Use Puppeteer to login to Leroy Merlin

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const {
   saveBills,
   log
 } = require('cozy-konnector-libs')
+
+const puppeteer = require('puppeteer');
+
 const request = requestFactory({
   // the debug mode shows all the details about http request and responses. Very usefull for
   // debugging but very verbose. That is why it is commented out by default
@@ -19,7 +22,8 @@ const request = requestFactory({
   jar: true
 })
 
-const baseUrl = 'http://books.toscrape.com'
+const baseUrl = 'https://www.leroymerlin.fr'
+const loginUrl = 'https://sso.leroymerlin.fr/oauth-server/oauth/authorize?language=fr&response_type=code&client_id=FRLM-FRONTWEB&state=weau6NTWeF&redirect_uri=https%3A%2F%2Fwww.leroymerlin.fr%2Foauthcallback%3Fclient_name%3DLmClient'
 
 module.exports = new BaseKonnector(start)
 
@@ -27,101 +31,51 @@ module.exports = new BaseKonnector(start)
 // information (fields). When you run this connector yourself in "standalone" mode or "dev" mode,
 // the account information come from ./konnector-dev-config.json file
 async function start(fields) {
+  const $ = await request(loginUrl)
+
   log('info', 'Authenticating ...')
-  await authenticate(fields.login, fields.password)
+  await authenticate(fields.login, fields.password, $)
   log('info', 'Successfully logged in')
+
   // The BaseKonnector instance expects a Promise as return of the function
-  log('info', 'Fetching the list of documents')
-  const $ = await request(`${baseUrl}/index.html`)
+  //log('info', 'Fetching the list of documents')
+  // const $ = await request(`${baseUrl}/index.html`)
   // cheerio (https://cheerio.js.org/) uses the same api as jQuery (http://jquery.com/)
-  log('info', 'Parsing list of documents')
-  const documents = await parseDocuments($)
+  //log('info', 'Parsing list of documents')
+  //const documents = await parseDocuments($)
 
   // here we use the saveBills function even if what we fetch are not bills, but this is the most
   // common case in connectors
-  log('info', 'Saving data to Cozy')
-  await saveBills(documents, fields.folderPath, {
-    // this is a bank identifier which will be used to link bills to bank operations. These
-    // identifiers should be at least a word found in the title of a bank operation related to this
-    // bill. It is not case sensitive.
-    identifiers: ['books']
-  })
+  //log('info', 'Saving data to Cozy')
+  //await saveBills(documents, fields.folderPath, {
+  // this is a bank identifier which will be used to link bills to bank operations. These
+  // identifiers should be at least a word found in the title of a bank operation related to this
+  // bill. It is not case sensitive.
+  //  identifiers: ['books']
+  //})
 }
 
 // this shows authentication using the [signin function](https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#module_signin)
 // even if this in another domain here, but it works as an example
-function authenticate(username, password) {
-  return signin({
-    url: `http://quotes.toscrape.com/login`,
-    formSelector: 'form',
-    formData: { username, password },
-    // the validate function will check if
-    validate: (statusCode, $) => {
-      // The login in toscrape.com always works excepted when no password is set
-      if ($(`a[href='/logout']`).length === 1) {
-        return true
-      } else {
-        // cozy-konnector-libs has its own logging function which format these logs with colors in
-        // standalone and dev mode and as JSON in production mode
-        log('error', $('.error').text())
-        return false
-      }
-    }
-  })
-}
+function authenticate(username, password, $) {
 
-// The goal of this function is to parse a html page wrapped by a cheerio instance
-// and return an array of js objects which will be saved to the cozy by saveBills (https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#savebills)
-function parseDocuments($) {
-  // you can find documentation about the scrape function here :
-  // https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#scrape
-  const docs = scrape(
-    $,
-    {
-      title: {
-        sel: 'h3 a',
-        attr: 'title'
-      },
-      amount: {
-        sel: '.price_color',
-        parse: normalizePrice
-      },
-      url: {
-        sel: 'h3 a',
-        attr: 'href',
-        parse: url => `${baseUrl}/${url}`
-      },
-      fileurl: {
-        sel: 'img',
-        attr: 'src',
-        parse: src => `${baseUrl}/${src}`
-      },
-      filename: {
-        sel: 'h3 a',
-        attr: 'title',
-        parse: title => `${title}.jpg`
-      }
-    },
-    'article'
-  )
-  return docs.map(doc => ({
-    ...doc,
-    // the saveBills function needs a date field
-    // even if it is a little artificial here (these are not real bills)
-    date: new Date(),
-    currency: '€',
-    vendor: 'template',
-    metadata: {
-      // it can be interesting that we add the date of import. This is not mandatory but may be
-      // usefull for debugging or data migration
-      importDate: new Date(),
-      // document version, usefull for migration after change of document structure
-      version: 1
-    }
-  }))
-}
+   (async () => {
+    const browser = await puppeteer.launch({ args: ['--no-sandbox']})
+    const page = await browser.newPage();
+    await page.goto(loginUrl, { waitUntil: 'load' });
 
-// convert a price string to a float
-function normalizePrice(price) {
-  return parseFloat(price.replace('£', '').trim())
+    const form = await page.$('#loginForm');
+    const usernameField = await form.$('#username')
+    const passwordField = await form.$('#password')
+    await usernameField.type(username);
+    await passwordField.type(password);
+    await form.press('Enter')
+
+    await page.waitForNavigation({ waitUntil: 'load' });
+    await page.screenshot({path: 'test.png'});
+
+    // TODO: resolve captcha
+
+    await browser.close();
+  })()
 }
